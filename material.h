d8d5a7acfad7e07d7c7fb1279b87a20c50bb57bb
Tunç Başar Köse <tuncbasark@gmail.com>
2023-10-22 00:46:23 +0300

    done

--- material.h
+++ material.h
@@ -19,7 +19,7 @@ class lambertian: public material {
         lambertian(const color& a): albedo(a) {}
 
         bool scatter(
-            const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
+            [[maybe_unused]] const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
         ) const override {
             auto scatter_direction = rec.normal + random_unit_vector(); 
 
